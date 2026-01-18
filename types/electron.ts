b4ac164c98b72e940e56114a74aa410e26c74132
Yukios <153779209+TahaNacibe@users.noreply.github.com>
2026-01-18 12:52:37 +0100

    Update electron.ts

--- types/electron.ts
+++ types/electron.ts
@@ -2,18 +2,5 @@ interface Window {
     electronAPI?: {
         // windows api control
         controlWindow: (action: 'minimize' | 'maximize' | 'close') => void;
-        
-        // files access api
-        filesAPI: (payload:
-            {
-                action: 'read' | 'createFolder' | 'createFile' | 'editFile' | 'delete',
-                dirPath: string,
-                options?: {
-                    folderName?: string,
-                    fileName?: string,
-                    content?: string,
-                    targetPath?: string
-                }
-            }) => Promise<{ success: boolean, data: any }>
     }
 }
